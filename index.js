9aaa03bbb6d0d79229a2b169267106e434458a53
jarbassbatista-bit <jarbas.sbatista@gmail.com>
2026-06-08 20:59:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 // CONFIGURAÇÕES — preencha com seus dados
 // ============================================================
 const CONFIG = {
-  ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,   // console.anthropic.com
+  //ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,   // console.anthropic.com
   ZAPI_INSTANCE:     "3F45B51C74DFF2AA7AA7FE743DE93AC5",   // z-api.io (opcional, ver README)
   ZAPI_TOKEN:        "32AB06403048DB069ED55973",
   SEU_NUMERO:        "5511992564805",           // seu número para notificações
